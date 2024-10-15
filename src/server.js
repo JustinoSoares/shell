d23fc40b33b5d9f7de52b890501a542be0272c1b
Justino Soares <justinocsoares123@gmail.com>
2024-10-15 13:29:04 +0100

    deploy

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('./router');
 const expressValidator = require("express-validator");
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 const app = express();
 app.use(express.json());
