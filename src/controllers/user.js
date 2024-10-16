e35d5f19acdf73bb2f8c37514b34141b27aad318
Justino Soares <justinocsoares123@gmail.com>
2024-10-16 18:45:21 +0100

    goo

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -194,7 +194,7 @@ module.exports = {
 
       return res
         .status(200)
-        .json({ status: 'true', msg: 'Login bem sucedido', token })
+        .json({ status: 'true', msg: 'Login bem sucedido', token, userId: user.id })
     } catch (error) {
       return res.status(401).json({
         status: 'error',
