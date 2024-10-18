30ffb34ae289d448ccbf953d0ecb83090ba25a4d
Justino Soares <justinocsoares123@gmail.com>
2024-10-18 04:29:47 +0100

    push

--- src/server.js
+++ src/server.js
@@ -4,29 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('./router');
 const expressValidator = require("express-validator");
-const port = process.env.PORT || 3000
-const { exec } = require('child_process');
-
-const fs = require('fs');
-const path = require('path');
-
-// Caminho absoluto para o script
-const scriptPath = path.resolve(__dirname, './script.sh');
-const testerPath = path.resolve(__dirname, '../upload/*.sh');
-
-// Tornar o arquivo executável
-fs.chmod(scriptPath, '755', (err) => {
-  if (err) throw err;
-  console.log('Arquivo agora é executável.');
-});
-
-exec(testerPath, (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(stdout);
-});
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
